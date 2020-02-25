97e825de96c1a998d64023bea69c70a51613e131
agney <agney1919@gmail.com>
2020-02-25 23:49:02 +0300

    Fix queue names + add pom.xml

--- src/main/java/miem/test/adapter/RouteConfiguration.java
+++ src/main/java/miem/test/adapter/RouteConfiguration.java
@@ -1,21 +1,15 @@
 package miem.test.adapter;
 
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.jms.admin.RMQConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.camel.Endpoint;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.component.rabbitmq.RabbitMQEndpoint;
-import org.apache.camel.spring.spi.TransactionErrorHandlerBuilder;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class RouteConfiguration extends BasicRouteConfiguration {
+
+    private static final String ROUTING_KEY_HEADER = "rabbitmq.ROUTING_KEY";
 
     @Override
     protected Logger getLogger() {
@@ -32,8 +26,9 @@ public class RouteConfiguration extends BasicRouteConfiguration {
         from("{{incoming.from.uri}}")
 //                .transacted()
                 .routeId("IN.MQ")
+                .setHeader(ROUTING_KEY_HEADER, simple("{{queue.out}}"))
                 .to("direct:prepareHeadersAndLog")
                 .to("{{outgoing.to.uri}}")
-                .log(LoggingLevel.INFO, getLogger(), "Successfully sent to destination");
+                .log(LoggingLevel.INFO, getLogger(), "Successfully sent to the destination");
     }
 }
